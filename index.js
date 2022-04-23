7ed975e04aeeca9e920a7ba203e54ee1d7cb490d
Victor Martins <vitor_jhoul@hotmail.com>
2022-04-23 17:43:45 -0300

    Finalizando projeto de consulta à API via cep

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
-'use strict';
-
+"use strict";
 
 const preencherFormulario = (endereco) => {
-    document.querySelector('#endereco').value = endereco.logradouro;
-    document.querySelector('#bairro').value = endereco.bairro;
-    document.querySelector('#cidade').value = endereco.localidade;
-    document.querySelector('#estado').value = endereco.uf;
-}
+  document.querySelector("#endereco").value = endereco.logradouro;
+  document.querySelector("#bairro").value = endereco.bairro;
+  document.querySelector("#cidade").value = endereco.localidade;
+  document.querySelector("#estado").value = endereco.uf;
+};
 
-const pesquisarCep = async() => {
-    const cep = document.querySelector('#cep').value;
-    const url = `http://viacep.com.br/ws/${cep}/json/`;
+const limparFormulario = (endereco) => {
+  document.querySelector("#endereco").value = "";
+  document.querySelector("#bairro").value = "";
+  document.querySelector("#estado").value = "";
+  document.querySelector("#cidade").value = "";
+};
+
+const cepValido = (cep) => cep.length == 8 && /^[0-9]+$/.test(cep);
+
+const pesquisarCep = async () => {
+  limparFormulario();
+  const cep = document.querySelector("#cep").value;
+  const url = `http://viacep.com.br/ws/${cep}/json/`;
+  if (cepValido(cep)) {
+    document.querySelector("#endereco").style.background = "#f7f6fa";
     const data = await fetch(url);
     const endereco = await data.json();
-    preencherFormulario(endereco);
-    
-    
-}
+    if (endereco.hasOwnProperty("erro")) {
+      document.querySelector("#endereco").style.background = "#ff000094";
+      document.querySelector("#endereco").value = "CEP não encontrado!";
+    } else {
+      preencherFormulario(endereco);
+      document.querySelector("#endereco").style.background = "#f7f6fa";
+    }
+  } else {
+    document.querySelector("#endereco").value = "CEP inválido!";
+    document.querySelector("#endereco").style.background = "#ff000094";
+  }
+};
 
-document.querySelector('#cep')
-    .addEventListener('focusout', pesquisarCep);
+document.querySelector("#cep").addEventListener("focusout", pesquisarCep);
